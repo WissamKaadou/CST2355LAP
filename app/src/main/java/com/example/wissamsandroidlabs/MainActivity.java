466c2781ed98db498d6095402b24690c642d613e
Wissam Kadou <kaad0004@algonquinlive.com>
2023-06-24 16:29:59 -0400

    Lab 4 Commit

--- app/src/main/java/com/example/wissamsandroidlabs/MainActivity.java
+++ app/src/main/java/com/example/wissamsandroidlabs/MainActivity.java
@@ -2,13 +2,59 @@ package com.example.wissamsandroidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        Button loginButton = findViewById(R.id.loginButton);
+        EditText editEmail = findViewById(R.id.editEmail);
+        loginButton.setOnClickListener( clk-> {
+            Intent nextPage = new Intent( MainActivity.this, SecondActivity.class);
+            nextPage.putExtra("EmailAddress", editEmail.getText().toString());
+            startActivity(nextPage);
+
+        } );
+
+
+        Log.w("MainActivity", "In onCreate() - Loading Widgets");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w("MainActivity", "In onDestroy() - Loading Widgets");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w("MainActivity", "In onPause() - Loading Widgets");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w("MainActivity", "In onStop() - Loading Widgets");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w("MainActivity", "In onResume() - Loading Widgets");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w("MainActivity", "In onStart() - Loading Widgets");
     }
 }
